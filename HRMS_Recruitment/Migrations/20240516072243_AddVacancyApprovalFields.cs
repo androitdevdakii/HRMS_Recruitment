de3c8f8bf1e2cdf1b6959a3ec35024a5c3c08429
Aaron Edaku <aaron.adroit@outlook.com>
2024-05-16 10:24:06 +0300

    HOD Dashboard Changes
    - Restrict Job Vacancies link to HODs
    
    - Add booleans to check if vacancy has been sent for approval
    
    - Create Migration to add booleans to vacany tables

---
+++ HRMS_Recruitment/Migrations/20240516072243_AddVacancyApprovalFields.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS_Recruitment.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVacancyApprovalFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubmittedToDirForApproval",
+                table: "JobPositionVacancy",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubmittedToHrForApproval",
+                table: "JobPositionVacancy",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubmittedToDirForApproval",
+                table: "JobPositionVacancy");
+
+            migrationBuilder.DropColumn(
+                name: "IsSubmittedToHrForApproval",
+                table: "JobPositionVacancy");
+        }
+    }
+}
--- HRMS_Recruitment/Models/JobPositionVacancy.cs
+++ HRMS_Recruitment/Models/JobPositionVacancy.cs
@@ -5,6 +5,8 @@ namespace HRMS_Recruitment.Models
     public class JobPositionVacancy
     {
         public int Id { get; set; }
+        public bool IsSubmittedToHrForApproval { get; set; }
+        public bool IsSubmittedToDirForApproval { get; set; }
         public bool IsHrApproved { get; set; }
         public bool IsDirApproved { get; set; }
         public bool IsPostedOnWebsite { get; set; }
@@ -14,6 +16,5 @@ namespace HRMS_Recruitment.Models
         public DateTime? ClosingDate { get; set; }
         public int JobPositionId { get; set; }
         public JobPosition? JobPosition { get; set; }
-
     }
 }
